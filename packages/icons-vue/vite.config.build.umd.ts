6d37d925352a57593ba98128a3dd82f3e444da43
richard1015 <51844712@qq.com>
2022-12-14 16:03:46 +0800

    release: @nutui/icons-vue 0.0.7

--- packages/icons-vue/vite.config.build.umd.ts
+++ packages/icons-vue/vite.config.build.umd.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/buildEntry/lib.ts'),
+      entry: resolve(__dirname, 'src/buildEntry/lib-new.ts'),
       name: 'index',
       // the proper extensions will be added
       fileName: 'index',
