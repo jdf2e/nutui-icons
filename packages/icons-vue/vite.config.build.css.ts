d151d97ca131b960a42a9a2772248eef068243cb
richard1015 <51844712@qq.com>
2022-12-19 10:43:43 +0800

    release: @nutui/icons-vue 0.0.13

---
+++ packages/icons-vue/vite.config.build.css.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import config from './package.json';
+
+const banner = `/*!
+* ${config.name} v${config.version} ${new Date()}
+* (c) 2022 @jdf2e.
+* Released under the MIT License.
+*/`;
+
+export default defineConfig({
+  build: {
+    minify: true,
+    lib: {
+      entry: '../../iconfont/iconfont_component.scss',
+      formats: ['es'],
+      name: 'style',
+      fileName: 'style'
+    },
+    // rollupOptions: {
+    //   output: {
+    //     banner
+    //   }
+    // },
+    emptyOutDir: false
+  }
+});
--- packages/icons-vue/vite.config.ts
+++ packages/icons-vue/vite.config.ts
@@ -5,4 +5,13 @@ import config from './package.json';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // example : additionalData: `@import "./src/design/styles/variables";`
+        // dont need include file extend .scss
+        additionalData: `@import "../../iconfont/iconfont_component.scss";`
+      }
+    }
+  }
 })
