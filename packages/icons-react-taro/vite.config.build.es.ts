297a64ec18e18900d06ec9cb4b1cc90f8e18e67a
xiaoyatong <84436086+xiaoyatong@users.noreply.github.com>
2026-08-07 09:52:48 +0800

    Feat v15 2 (#39)
    * feat: 升级15icon库
    
    * feat: 增加适配
    
    * release: @nutui/icons-react-taro 3.0.2-beta.1
    
    * fix: 修改为一个更语义化的名字
    
    * release: @nutui/icons-react-taro 3.0.2-beta.2
    
    * release: @nutui/icons-taro 3.0.2-beta.2
    
    * release: @nutui/icons-taro 3.0.2-beta.3
    
    * release: @nutui/icons-taro 3.0.2-beta.4
    
    * release: @nutui/icons-taro 3.0.2-beta.5
    
    * release: @nutui/icons-react-taro 3.0.2-beta.3
    
    * fix: icons svg 命名
    
    * release: @nutui/icons-react-taro 3.0.2-beta.ios.0
    
    * release: @nutui/icons-react-taro 3.0.2-beta.ios.1
    
    * release: @nutui/icons-react-taro 3.0.2-beta.ios.2
    
    * release: @nutui/icons-react-taro 3.0.2-cpp.1
    
    * chore: 发布最新版本
    
    * release: @nutui/icons-react-taro 3.0.2-cpp.2
    
    * fix: 兼容nextjs ssr (#40)
    
    不加的话打包产物会有`var w = F.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;`这样的代码
    
    * feat: 修改arrow-radius图片
    
    * fix: icon色值修订,会影响checkbox
    
    ---------
    
    Co-authored-by: tongen <tongen@jd.com>
    Co-authored-by: KouSum <39258402+KouSum@users.noreply.github.com>
    Co-authored-by: songchenglin3 <songchenglin1@jd.com>

--- packages/icons-react-taro/vite.config.build.es.ts
+++ packages/icons-react-taro/vite.config.build.es.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import {dirname, resolve, join} from 'path'
-import react from '@vitejs/plugin-react'
-import {iconsConfig} from './src/components/iconsConfig'
+import { defineConfig } from "vite";
+import { dirname, resolve, join } from "path";
+import react from "@vitejs/plugin-react";
+import { iconsConfig } from "./src/components/iconsConfig";
 import * as fs from "fs";
 
 let input = {
@@ -19,41 +19,55 @@ iconsConfig.map((name) => {
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: false,
-  plugins: [react({jsxRuntime: 'classic'}), {
-    name: 'revert Process.env',
-    apply: 'build',
-    async closeBundle() {
-      const esFile = join(__dirname, 'dist/es/icons/IconHarmonyTemplate.js')
-      const fileContent = fs.readFileSync(esFile).toString().replace(`(void 0).TARO_ENV === "h5"`, `process.env.TARO_ENV === "h5"`)
-      fs.writeFileSync(esFile, fileContent)
-      console.log('File rewritten successfully!');
-    }
-  }],
+  plugins: [
+    react({ jsxRuntime: "classic" }),
+    {
+      name: "revert Process.env",
+      apply: "build",
+      async closeBundle() {
+        const esFile = join(__dirname, "dist/es/icons/IconHarmonyTemplate.js");
+        const fileContent = fs
+          .readFileSync(esFile)
+          .toString()
+          .replace(
+            `(void 0).TARO_ENV === "h5"`,
+            `process.env.TARO_ENV === "h5"`
+          );
+        fs.writeFileSync(esFile, fileContent);
+        console.log("File rewritten successfully!");
+      },
+    },
+  ],
   build: {
     minify: false,
     lib: {
       entry: input,
-      formats: ['es']
+      formats: ["es"],
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['react', 'react-dom', 'classnames', './internal', './configure'],
+      external: [
+        "react",
+        "react-dom",
+        "classnames",
+        "./internal",
+        "./configure",
+        "@tarojs/components",
+      ],
       // input,
       output: {
         paths: (id) => {
-          return /internal/.test(id)
-              ? `./internal.js`
-              : id
+          return /internal/.test(id) ? `./internal.js` : id;
         },
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        dir: resolve(__dirname, './dist/es/icons'),
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        dir: resolve(__dirname, "./dist/es/icons"),
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
-})
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
